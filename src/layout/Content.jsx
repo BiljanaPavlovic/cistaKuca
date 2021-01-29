ed3c1de7918a42cad5bacf3734bfb5358bc7fcfc
Biljana <zabiljanupavlovic@gmail.com>
2021-01-29 01:30:15 +0100

    design changes

--- src/layout/Content.jsx
+++ src/layout/Content.jsx
@@ -14,27 +14,44 @@ export default function Content() {
   return (
     <div>
       <div className="dashboard">
-        <div className="dashboard">
-          <p className="content-p">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
-            nemo blanditiis eaque rerum fugiat corrupti! Voluptatibus ducimus
-          </p>
-          <Link to="/usluge">
-            <Button variant="primary" className="btn" size="lg">
-              Usluge Čišćenja
-            </Button>{" "}
-          </Link>
+        <div className="one-section">
+          <img
+            src="/cistaKuca/img/zenaCisti.gif"
+            className="lp-img"
+            alt="zena cist"
+          />
+          <div className="btn-p">
+            <p className="content-p">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
+              nemo blanditiis eaque rerum fugiat corrupti! Voluptatibus ducimus
+            </p>
+            <Link to="/usluge">
+              <Button variant="primary" className="btn" size="lg">
+                Usluge Čišćenja
+              </Button>{" "}
+            </Link>
+          </div>
         </div>
         <div className="dashboard">
-          <Link to="/angazman">
-            <Button variant="secondary" className="btn" size="lg">
-              Angažman
-            </Button>
-          </Link>
-          <p className="content-p">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores
-            nemo blanditiis eaque rerum fugiat corrupti! Voluptatibus ducimus
-          </p>
+          <div className="one-section">
+            <div className="btn-p">
+              <Link to="/angazman">
+                <Button variant="secondary" className="btn" size="lg">
+                  Angažman
+                </Button>
+              </Link>
+              <p className="content-p">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Maiores nemo blanditiis eaque rerum fugiat corrupti!
+                Voluptatibus ducimus
+              </p>
+            </div>
+            <img
+              src="/cistaKuca/img/potirePod.gif"
+              className="lp-img"
+              alt="ciscenje poslovnog prostora"
+            />
+          </div>
         </div>
         <hr />
       </div>
