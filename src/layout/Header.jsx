6f9a64f70f0f094ef457bb1b1a4b2d84c88ebe50
Biljana <zabiljanupavlovic@gmail.com>
2021-01-29 17:53:48 +0100

    some design changes

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -8,6 +8,7 @@ export default function Header() {
   return (
     <div className="header">
       <Link to="/">
+        <img src="/cistaKuca/img/ckl.png" alt="logo" className="logo" />
         <h1> Čista kuca</h1>
       </Link>
       <br />
